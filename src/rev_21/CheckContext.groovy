b753271074a9936b7216ed5b18f57c5ef0dd350a
sklawonn <115167993+sklawonn@users.noreply.github.com>
2025-04-01 09:38:45 +0200

    Create CheckContext.groovy
    Update CheckContext.groovy

---
+++ src/rev_21/CheckContext.groovy
@@ -0,0 +1,28 @@
+/*
+ The integration developer needs to create the method processData 
+ This method takes Message object of package com.sap.gateway.ip.core.customdev.util 
+which includes helper methods useful for the content developer:
+The methods available are:
+    public java.lang.Object getBody()
+	public void setBody(java.lang.Object exchangeBody)
+    public java.util.Map<java.lang.String,java.lang.Object> getHeaders()
+    public void setHeaders(java.util.Map<java.lang.String,java.lang.Object> exchangeHeaders)
+    public void setHeader(java.lang.String name, java.lang.Object value)
+    public java.util.Map<java.lang.String,java.lang.Object> getProperties()
+    public void setProperties(java.util.Map<java.lang.String,java.lang.Object> exchangeProperties) 
+    public void setProperty(java.lang.String name, java.lang.Object value)
+    public java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> getSoapHeaders()
+    public void setSoapHeaders(java.util.List<com.sap.gateway.ip.core.customdev.util.SoapHeader> soapHeaders) 
+       public void clearSoapHeaders()
+ */
+import com.sap.gateway.ip.core.customdev.util.Message;
+def Message processData(Message message) {
+       //Headers 
+       def header = message.getHeaders();
+       def context = header.get("context");
+       if (context==null){
+        context = "Result"
+        }
+       message.setHeader("context", context);
+       return message;
+}
